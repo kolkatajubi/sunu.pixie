6eed6aee3b27c6ad3f025fcb5a47e46fddf6a1ce
kolkatajubi <kolkatachamps@jubi.ai>
2019-09-12 05:16:13 -0700

    msg

--- assets/action.js
+++ assets/action.js
@@ -1,11 +1,20 @@
 
-  
+  //REFERENCE
+  // let signupStatus= await pixieapi.signup("sonu","1234","1234","q45@hX")
+  // console.log(JSON.stringify(signupStatus))// {"status":"success"}
+  // let signinStatus = await pixieapi.signin("sonu","1234");
+  // console.log(JSON.stringify(signinStatus))//{"status":"success","data":{"_id":"5d7a2e5de3ef311b0fb9c378","username":"sonu","referral":"bcb97414-384f-46cc-8466-35abc0686f53","__v":0,"sessionId":"ba9fd4ed-1254-482c-9988-66202b622f80"}}
+  // let loginStatus=await pixieapi.checkLogin();
+  // console.log(JSON.stringify(loginStatus))//{"status":"success"}
+ 
 $(document).ready(async ()=> {
-  let signupStatus= await pixieapi.signup("sonu","1234","1234","q45@hX")
-  console.log(JSON.stringify(signupStatus))
-  let signinStatus = await pixieapi.signin("sonu","1234");
-  console.log(JSON.stringify(signinStatus))
-  let loginStatus=await pixieapi.checkLogin();
-  console.log(JSON.stringify(loginStatus))
+  let loginStatus=await pixieapi.checkLogin()
+  if(loginStatus&&loginStatus.status=="success"){
+    window.location.replace("https://pixie.jubi.ai/chat");
+  }
+  else{
+    $("#loading-div").hide()
+    $("#signin-div").show()
+  }
 });
   
