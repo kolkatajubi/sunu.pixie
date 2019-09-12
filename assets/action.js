377fe2721a393d9f6c413342b8aad4f00e2dc19d
kolkatajubi <kolkatachamps@jubi.ai>
2019-09-12 05:11:51 -0700

    msg

--- assets/action.js
+++ assets/action.js
@@ -1,7 +1,11 @@
 
   
 $(document).ready(async ()=> {
+  let signupStatus= await pixieapi.signup("sonu","1234","1234","q45@hX")
+  console.log(JSON.stringify(signupStatus))
+  let signinStatus = await pixieapi.signin("sonu","1234");
+  console.log(JSON.stringify(signinStatus))
   let loginStatus=await pixieapi.checkLogin();
-  console.log(loginStatus)
+  console.log(JSON.stringify(loginStatus))
 });
   
