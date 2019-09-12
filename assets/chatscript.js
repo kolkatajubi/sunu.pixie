34f35003a102a3a5e155fa32ac0bc62c715eaf2b
kolkatajubi <kolkatachamps@jubi.ai>
2019-09-12 03:52:07 -0700

    msg

--- assets/chatscript.js
+++ assets/chatscript.js
@@ -1,19 +1,15 @@
-$(document).ready(function(){
     // $(".signup").hide();
     //   $(".i").click(function(){
     //     $(".signup").show();
     //     $(".signin").hide();  
     // });
     // });
-
-  ({
-    
+    jQuery.ajax({
+      type: "POST",
       beforeSend: function() {
-          $(".sec_loginform").hide();
-      },
-      success: function(data) {
           $(".sec_loginform").show();
-      
-        }
-      });
-      });
+      },
+      success: function() {
+          $(".sec_loginform").hide();
+      }
+  });
