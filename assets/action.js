9261321d19cd70f3ac2e20b4bcf5b726bcd507c9
kolkatajubi <kolkatachamps@jubi.ai>
2019-09-12 05:35:23 -0700

    msg

--- assets/action.js
+++ assets/action.js
@@ -7,14 +7,57 @@
   // let loginStatus=await pixieapi.checkLogin();
   // console.log(JSON.stringify(loginStatus))//{"status":"success"}
  
-$(document).ready(async ()=> {
-  let loginStatus=await pixieapi.checkLogin()
-  if(loginStatus&&loginStatus.status=="success"){
-    window.location.replace("https://pixie.jubi.ai/chat");
-  }
-  else{
-    $("#loading-div").hide()
+$(document).ready(()=> {
+  var user={}
+
+  //on load
+  ;(async()=>{
+    let loginStatus=await pixieapi.checkLogin()
+    if(loginStatus&&loginStatus.status=="success"){
+      window.location.replace("https://pixie.jubi.ai/chat");
+    }
+    else{
+      $("#loading-div").hide()
+      $("#signin-div").show()
+    }
+  })();
+
+  //redirects
+  $("#redirect-sign-in").click(()=>{
     $("#signin-div").show()
-  }
+    $("#signup-div").hide()
+  })
+  $("#redirect-sign-up").click(()=>{
+    $("#signin-div").hide()
+    $("#signup-div").show()
+  })
+
+  //on signin
+  $("#sign-in").click(async()=>{
+    let username=$("#signin-username-input").val()
+    let password=$("#signin-password-input").val()
+    let signinStatus = await pixieapi.signin(username,password);
+    if (signinStatus.status=="success"&&signinStatus.data&&signinStatus.data.username==username){
+      user=signinStatus.data
+      window.location.replace("https://pixie.jubi.ai/chat");
+      return
+    }
+    $("#signin-error").val(signinStatus.data)
+  })
+
+  //on signup
+  $("#sign-up").click(async()=>{
+    let username=$("#signup-username-input").val()
+    let password=$("#signup-password-input").val()
+    let cPassword=$("#signup-confirmpassword-input").val()
+    let referral=$("#signup-referral-input").val()
+    let signupStatus = await pixieapi.signup(username,password,cPassword,referral);
+    if (signupStatus.status=="success"){
+      $("#signin-div").show()
+      $("#signup-div").hide()
+    }
+    $("#signup-error").val(signupStatus.data)
+  })
+
 });
   
